1c8052ca2ff008ac52c56062adff4c18f46fa611
Alana Rodrigues <alana.oliveira.rodrigues@gmail.com>
2024-05-25 22:05:01 -0300

    rabbit background

--- src/Motos.Consumer/RabbitMQBackgroundService.cs
+++ src/Motos.Consumer/RabbitMQBackgroundService.cs
@@ -1,10 +1,15 @@
-﻿using Motos.Data;
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Motos.Data.Entities;
+using Motos.Data;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using System.Text;
-
-namespace Motos.Consumer;
+using RabbitMQ.Client.Exceptions;
 
 public class RabbitMQBackgroundService : BackgroundService
 {
@@ -14,29 +19,95 @@ public class RabbitMQBackgroundService : BackgroundService
     private readonly int _rabbitMQPort;
     private readonly string _rabbitMQUser;
     private readonly string _rabbitMQPassword;
-    private readonly IMotosRepository motosRepository;
+    private IConnection _connection;
+    private IModel _channel;
 
     public RabbitMQBackgroundService(
         ILogger<RabbitMQBackgroundService> logger,
         IServiceScopeFactory serviceScopeFactory,
-        IMotosRepository motosRepository)
+        string rabbitMQHost,
+        int rabbitMQPort,
+        string rabbitMQUser,
+        string rabbitMQPassword)
     {
-
-        var rabbitMQHost = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
-        var rabbitMQPort = int.Parse(Environment.GetEnvironmentVariable("RABBITMQ_PORT"));
-        var rabbitMQUser = Environment.GetEnvironmentVariable("RABBITMQ_USER");
-        var rabbitMQPassword = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
-
         _logger = logger;
         _serviceScopeFactory = serviceScopeFactory;
         _rabbitMQHost = rabbitMQHost;
         _rabbitMQPort = rabbitMQPort;
         _rabbitMQUser = rabbitMQUser;
         _rabbitMQPassword = rabbitMQPassword;
-        this.motosRepository = motosRepository;
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.Register(() => _logger.LogInformation("RabbitMQ background task is stopping."));
+
+        return Task.Run(async () => {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    if (_connection == null || !_connection.IsOpen)
+                    {
+                        ConnectToRabbitMQ();
+                    }
+
+                    if (_channel == null || !_channel.IsOpen)
+                    {
+                        _channel = _connection.CreateModel();
+                        _channel.QueueDeclare(queue: "motos_queue",
+                                             durable: true,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+
+                        var consumer = new EventingBasicConsumer(_channel);
+                        consumer.Received += async (model, ea) =>
+                        {
+                            var body = ea.Body.ToArray();
+                            var message = Encoding.UTF8.GetString(body);
+                            _logger.LogInformation($"Received message: {message}");
+
+                            using (var scope = _serviceScopeFactory.CreateScope())
+                            {
+                                var motosRepository = scope.ServiceProvider.GetRequiredService<IMotosRepository>();
+                                await motosRepository.SaveMoto2024Async(new MotosLog2024(message));
+                            }
+
+                            // Acknowledge the message
+                            _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                        };
+
+                        _channel.BasicConsume(queue: "motos_queue",
+                                             autoAck: false,
+                                             consumer: consumer);
+                    }
+
+                    await Task.Delay(5000, stoppingToken); // Wait for 5 seconds before checking again
+                }
+                catch (BrokerUnreachableException ex)
+                {
+                    _logger.LogError(ex, "RabbitMQ BrokerUnreachableException: Unable to reach RabbitMQ broker. Retrying in 5 seconds...");
+                    await Task.Delay(5000, stoppingToken); // Wait before retrying to connect
+                }
+                catch (AlreadyClosedException ex)
+                {
+                    _logger.LogError(ex, "RabbitMQ connection was closed. Reconnecting...");
+                    DisposeConnectionAndChannel();
+                    await Task.Delay(5000, stoppingToken); // Wait before retrying to connect
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "An unexpected error occurred.");
+                    await Task.Delay(5000, stoppingToken); // Wait before retrying on general error
+                }
+            }
+
+            DisposeConnectionAndChannel();
+        }, stoppingToken);
+    }
+
+    private void ConnectToRabbitMQ()
     {
         var factory = new ConnectionFactory()
         {
@@ -46,36 +117,30 @@ public class RabbitMQBackgroundService : BackgroundService
             Password = _rabbitMQPassword,
         };
 
-        using (var connection = factory.CreateConnection())
-        using (var channel = connection.CreateModel())
+        _connection = factory.CreateConnection();
+        _logger.LogInformation("RabbitMQ connection established.");
+    }
+
+    private void DisposeConnectionAndChannel()
+    {
+        if (_channel != null)
         {
-            channel.QueueDeclare(queue: "motos_queue",
-                                    durable: true,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
-
-            var consumer = new EventingBasicConsumer(channel);
-
-            consumer.Received += async (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                _logger.LogInformation($"Received message: {message}");
-
-                await motosRepository.SaveMoto2024Async(new MotosLog2024(message));
-
-                // Acknowledge the message
-                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-            };
-
-            await motosRepository.SaveMoto2024Async(new MotosLog2024("message"));
-
-            channel.BasicConsume(queue: "motos_queue",
-                                    autoAck: true,
-                                    consumer: consumer);
-
-            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken); // Wait for 5 seconds before reconnecting
+            _channel.Close();
+            _channel.Dispose();
+            _channel = null;
         }
+
+        if (_connection != null)
+        {
+            _connection.Close();
+            _connection.Dispose();
+            _connection = null;
+        }
+    }
+
+    public override void Dispose()
+    {
+        DisposeConnectionAndChannel();
+        base.Dispose();
     }
 }
--- src/Motos.Consumer/Startup.cs
+++ src/Motos.Consumer/Startup.cs
@@ -9,17 +9,37 @@ public class Startup
 
     public Startup(IConfiguration configuration)
         => Configuration = configuration;
-
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddScoped<DbContext, MotosContext>();
+        services.AddLogging(config =>
+        {
+            config.AddConsole();
+            config.SetMinimumLevel(LogLevel.Information);
+        });
 
+        services.AddSingleton<IMotosRepository, MotosRepository>();
         services.AddDbContext<MotosContext>(options =>
-            options.UseNpgsql(
-                Configuration.GetConnectionString("DefaultConnection")).EnableDetailedErrors());
+            options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString__DefaultConnection")));
 
-        services.AddHostedService<RabbitMQBackgroundService>();
+        services.AddHostedService<RabbitMQBackgroundService>(provider =>
+        {
+            var logger = provider.GetRequiredService<ILogger<RabbitMQBackgroundService>>();
+            var serviceScopeFactory = provider.GetRequiredService<IServiceScopeFactory>();
+            var rabbitMQHost = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
+            var rabbitMQPort = int.Parse(Environment.GetEnvironmentVariable("RABBITMQ_PORT"));
+            var rabbitMQUser = Environment.GetEnvironmentVariable("RABBITMQ_USER");
+            var rabbitMQPassword = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
 
-        services.AddScoped<IMotosRepository, MotosRepository>();
+            return new RabbitMQBackgroundService(
+                logger,
+                serviceScopeFactory,
+                rabbitMQHost,
+                rabbitMQPort,
+                rabbitMQUser,
+                rabbitMQPassword
+            );
+        });
     }
+
+
 }
