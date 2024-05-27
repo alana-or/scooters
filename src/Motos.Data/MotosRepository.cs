c75209da5273e3796cd5d3aa4929374a02f9ed18
Alana Rodrigues <alana.oliveira.rodrigues@gmail.com>
2024-05-27 00:15:32 -0300

    Add testes

--- src/Motos.Data/MotosRepository.cs
+++ src/Motos.Data/MotosRepository.cs
@@ -88,7 +88,7 @@ public class MotosRepository : IMotosRepository
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "An error occurred while creating motos log.");
+            logger.LogError(ex, "An error occurred while creating moto log.");
             throw;
         }
     }
